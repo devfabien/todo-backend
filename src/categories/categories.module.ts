813514e05751a77b5f38df2dc6468cbbccd194f3
devfabien <ish001fabi@gmail.com>
2023-12-12 12:39:08 +0200

    feat: creating the categories module and adding the providers

---
+++ src/categories/categories.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CategoriesController } from './categories.controller';
+import { Category } from './entity/category.entity';
+import { JsonDbRepository } from 'src/db/json-db-repository';
+
+@Module({
+  imports: [],
+  providers: [
+    CategoriesService,
+    {
+      provide: 'CategoryRepository',
+      useFactory: () => new JsonDbRepository<Category>('categories'),
+    },
+  ],
+  controllers: [CategoriesController],
+  exports: [CategoriesService],
+})
+export class CategoriesModule {}
