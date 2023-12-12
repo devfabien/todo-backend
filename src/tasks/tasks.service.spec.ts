5741dc54967579f8c53145e523bc6676c069247b
devfabien <ish001fabi@gmail.com>
2023-12-12 18:26:59 +0200

    feat: creating the test to check if the task service is defined

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,18 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { JsonDbRepository } from 'src/db/json-db-repository';
+import { Task, TaskStatus } from './entity/task.entity';
+import { CategoriesService } from '../categories/categories.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 describe('TasksService', () => {
-  let service: TasksService;
+  let taskService: TasksService;
+  let repository: JsonDbRepository<Task>;
+  let categoryService: CategoriesService;
+
+  const mockTask = {
+    id: 'e16328dd-c325-4098-b5a8-3002c3915813',
+    title: 'eat',
+    description: 'i have to eat today',
+    categoryId: '50376a95-11fb-4310-a36c-b08c6c537874',
+    status: TaskStatus.OPEN,
+  };
+
+  const mockService = {
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      imports: [],
+      providers: [
+        TasksService,
+        CategoriesService,
+        {
+          provide: 'TaskRepository',
+          useValue: mockService,
+        },
+        {
+          provide: 'CategoryRepository',
+          useValue: mockService,
+        },
+      ],
     }).compile();
 
-    service = module.get<TasksService>(TasksService);
+    taskService = module.get<TasksService>(TasksService);
+    categoryService = module.get<CategoriesService>(CategoriesService);
+    repository = module.get<JsonDbRepository<Task>>('TaskRepository');
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(taskService).toBeDefined();
   });
 });
