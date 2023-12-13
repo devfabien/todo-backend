a7722f6ab5d34f4a315e542075439c3207bde124
devfabien <ish001fabi@gmail.com>
2023-12-13 18:36:12 +0200

    refactor: updating the dto response

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,8 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCategoryDto {
   readonly id: string;
 
+  @ApiProperty({
+    title: 'name example',
+  })
   @IsNotEmpty()
   @IsString()
   readonly name: string;
