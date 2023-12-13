e75ad4c201fe14575223fb86a85102f75f0bb98f
devfabien <ish001fabi@gmail.com>
2023-12-13 18:07:29 +0200

    refactor: modifying the test for app controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "APIs for todo app"', () => {
+      expect(appController.getHello()).toBe('APIs for todo app');
     });
   });
 });
