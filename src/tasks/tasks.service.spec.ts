0a3e69785b09d16138931ae1e80cb0666af2440c
devfabien <ish001fabi@gmail.com>
2023-12-13 16:14:55 +0200

    refactor: removing the integration with categories service

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -2,14 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
 import { JsonDbRepository } from 'src/db/json-db-repository';
 import { Task, TaskStatus } from './entity/task.entity';
-import { CategoriesService } from '../categories/categories.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { NotFoundException } from '@nestjs/common';
 
 describe('TasksService', () => {
   let taskService: TasksService;
   let repository: JsonDbRepository<Task>;
-  let categoryService: CategoriesService;
 
   const mockTask = {
     id: 'e16328dd-c325-4098-b5a8-3002c3915813',
@@ -30,20 +28,14 @@ describe('TasksService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         TasksService,
-        CategoriesService,
         {
           provide: 'TaskRepository',
-          useValue: mockService,
-        },
-        {
-          provide: 'CategoryRepository',
           useValue: mockService,
         },
       ],
     }).compile();
 
     taskService = module.get<TasksService>(TasksService);
-    categoryService = module.get<CategoriesService>(CategoriesService);
     repository = module.get<JsonDbRepository<Task>>('TaskRepository');
   });
 
@@ -81,25 +73,13 @@ describe('TasksService', () => {
       description: 'i have to eat today',
       categoryId: '50376a95-11fb-4310-a36c-b08c6c537874',
     };
-    const mockCategory = {
-      id: '50376a95-11fb-4310-a36c-b08c6c537874',
-      name: 'first',
-    };
 
     it('should create a new task', async () => {
-      jest
-        .spyOn(categoryService, 'findOneCategory')
-        .mockResolvedValue(mockCategory);
       jest.spyOn(repository, 'create').mockResolvedValue(mockTask);
 
       const result = await taskService.create(newTask as CreateTaskDto);
 
       expect(result).toEqual(mockTask);
-    });
-    it('should should throw a NotFoundException if category not found', async () => {
-      jest.spyOn(repository, 'findOne').mockResolvedValue(null);
-      const result = categoryService.findOneCategory(newTask.categoryId);
-      expect(result).rejects.toThrow(NotFoundException);
     });
   });
   describe('delete', () => {
